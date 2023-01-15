87723578c5f56ce2e25f47dc33bb708fd0638e08
xStef23 <trstefy@gmail.com>
2023-01-15 15:26:33 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import CharsGrid from "./components/CharsGrid";
+import axios from "axios";
 
 function App() {
   const [chars, setChars] = useState([]);
@@ -8,16 +9,23 @@ function App() {
   const [page, setPage] = useState(1);
 
   useEffect(() => {
-    (async function getData() {
-      await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
-        .then((response) => response.json())
-        .then((json) => {
-          setChars(json.results);
-        });
-    })();
-  }, [page]);
+    axios
+      .get(`https://rickandmortyapi.com/api/character?page=${page}`)
+      .then((response) => {
+        const chars = response.data.results;
+        const info = response.data.info;
+        setChars(chars);
+        //console.log(info.prev);
 
-  console.log(page);
+        info.prev === null
+          ? (document.getElementById("btnPrev").style.display = "none")
+          : (document.getElementById("btnPrev").style.display = "block");
+
+        info.next === null
+          ? (document.getElementById("btnNext").style.display = "none")
+          : (document.getElementById("btnNext").style.display = "block");
+      });
+  }, [page]);
 
   function handleNextPage() {
     const currentPage = page + 1;
@@ -32,12 +40,13 @@ function App() {
 
   return (
     <div className="App">
+      <h1>The Rick and Morty API</h1>
       <CharsGrid chars={chars}></CharsGrid>
-      <div className="my-5">
+      <div className="my-5 flex">
         <button
           className="bg-blue-500 disabled hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-4 "
           onClick={handlePreviousPage}
-          disabled={page === 1}
+          id="btnPrev"
         >
           PREVIOUS PAGE
         </button>
@@ -45,6 +54,7 @@ function App() {
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={handleNextPage}
+          id="btnNext"
         >
           NEXT PAGE
         </button>
